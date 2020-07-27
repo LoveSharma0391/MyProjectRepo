75a95f099b65382a2b1ae19ea941e1622780b99c
LoveSharma0391 <lovesharma12032020@gmail.com>
2020-07-27 05:57:16 -0700

    update upto 27.07.2020

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About=()=>{
+    //console.log(props)
+    // setTimeout(()=>{
+    //   props.history.push('/')
+    // },3000) 
+    return(
+      <div>      
+   <h1>i am about page</h1>
+   {/* <button onClick={()=>{props.history.push('/')}}>
+     let's navigate 
+   </button> */}
+      </div>
+     
+    )
+  }
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home=()=>{
+    console.log()
+  return(
+    <h1>i am home page</h1>
+  )
+}
+
+export default Home;
